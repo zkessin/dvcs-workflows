d153565c4c74955b313288fc7abd8bb8289c6d46
Zachary Kessin <zkessin@gmail.com>
2012-08-07 10:30:16 +0300

    build script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ for chapter in `ls *.asciidoc`
 do 
     asciidoc --backend=html4 --out-file=html/$chapter.html $chapter
 done
-cp html/book.doc.html html/index.html
+cp html/book.asciidoc.html html/index.html
