010441d54b4ffe4c05f214c454f479960b21c421
Zachary Kessin <zkessin@gmail.com>
2012-08-26 21:25:37 -0400

    html5 backend

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 for chapter in `ls *.asciidoc` 
 do 
-    asciidoc --backend=html4 --out-file=html/$chapter.html $chapter
+    asciidoc --backend=html5 --out-file=html/$chapter.html $chapter
 done
 cp html/book.asciidoc.html html/index.html
