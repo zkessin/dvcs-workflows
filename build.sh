af3eac83477c3d9bc4acf3d6711d34c6e49638a6
Zachary Kessin <zkessin@gmail.com>
2012-09-25 13:33:07 +0200

    fixed the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-for chapter in `ls *.asciidoc` 
-do 
-    asciidoc --backend=html5 --out-file=html/$chapter.html $chapter
-done
-cp html/book.asciidoc.html html/index.html
+asciidoc --backend=html5 --out-file=book.html toc.asciidoc
