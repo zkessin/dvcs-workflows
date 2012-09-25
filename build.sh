fd57d1962e0d1cdc466076fe2ef03041ed248986
Zachary Kessin <zkessin@gmail.com>
2012-09-25 15:54:37 +0200

    local build tool

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-asciidoc --backend=html5 --out-file=book.html toc.asciidoc
+TARGET=/Users/zkessin/Documents/sandbox/dvcs-workflows/
+asciidoc --backend=html5 --out-file=index.html toc.asciidoc
+cp index.html $TARGET
+cp -r dvcs_cloud_hosting $TARGET
+cp -r graphical_git $TARGET
+cp -r pull_request $TARGET
+cp -r using_hg  $TARGET
