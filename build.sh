90297e1ba8c762793e79235ecda8f075a307077c
Zachary Kessin <zkessin@gmail.com>
2012-09-28 08:13:56 +0200

    simplify the script a lot

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-TARGET=/Users/zkessin/Documents/sandbox/dvcs-workflows/
-ORIG=`pwd`
+
 asciidoc --backend=html5 --out-file=index.html toc.asciidoc
-cp index.html $TARGET
-cp -r dvcs_cloud_hosting $TARGET
-cp -r graphical_git $TARGET
-cp -r pull_request $TARGET
-cp -r using_hg  $TARGET
-cd $TARGET
-git commit -m "Build of HTML"
-cd $ORIG
-git fetch ~/Documents/sandbox/dvcs-workflows/ gh-pages
+git commit -m "Build of HTML" index.html
